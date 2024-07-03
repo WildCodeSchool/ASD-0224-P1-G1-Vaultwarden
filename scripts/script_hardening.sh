7aec834ca0c237b4329618d2fe04d9d56b2e119a
Vehementz <vale-tudo91@sfr.fr>
2024-07-03 16:02:08 +0200

    standart edition

--- scripts/script_hardening.sh
+++ scripts/script_hardening.sh
@@ -474,17 +474,160 @@ harden_sshd_config() {
     fi
 
     settings_ssh=(
+        [ForwardAgent]="yes"
+        [ForwardX11]="no"
+        [ForwardX11Trusted]="no"
+        [PasswordAuthentication]="no"
+        [HostbasedAuthentication]="no" # no verified     
+        #   GSSAPIAuthentication no
+        #   GSSAPIDelegateCredentials n
+        #   GSSAPIKeyExchange no
+        #   GSSAPITrustDNS no
+        #   BatchMode no
+        #   CheckHostIP yes
+        #   AddressFamily any
+        #   ConnectTimeout 0
+        #   StrictHostKeyChecking ask
+        #   IdentityFile ~/.ssh/id_rsa
+        #   IdentityFile ~/.ssh/id_dsa
+        #   IdentityFile ~/.ssh/id_ecds
+        #   IdentityFile ~/.ssh/id_ed25
+        [Port]="$PORT"
+        #   Ciphers aes128-ctr,aes192-c
+        #   MACs hmac-md5,hmac-sha1,uma
+        #   EscapeChar ~
+            [Tunnel]="no"
+            [TunnelDevice]="any:any"
+        #   PermitLocalCommand no
+        #   VisualHostKey no
+        #   ProxyCommand ssh -q -W %h:%
+        #   RekeyLimit 1G 1h
+        #   SendEnv LANG LC_*
+        #   HashKnownHosts yes
+        #   GSSAPIAuthentication yes
+    )
+
+        settings_ssh_daemon=(
+        [Port]="$PORT"
+        #AddressFamily any
+        #ListenAddress 0.0.0.0
+        #ListenAddress ::
+
+        #HostKey /etc/ssh/ssh_host_rsa_key
+        #HostKey /etc/ssh/ssh_host_ecdsa_key
+        #HostKey /etc/ssh/ssh_host_ed25519_key
+
+        # Ciphers and keying
+        #RekeyLimit default none
+
+        # Logging
+        #SyslogFacility AUTH
+        [LogLevel]="VERBOSE"
+
+        # Authentication:
+
+        #LoginGraceTime 2m
+        #PermitRootLogin prohibit-password
+        #StrictModes yes
+        #MaxAuthTries 6
+        #MaxSessions 10
+
+        #PubkeyAuthentication yes
+
+        # Expect .ssh/authorized_keys2 to be disregarded by default in future.
+        #AuthorizedKeysFile	.ssh/authorized_keys .ssh/authorized_keys2
+
+        #AuthorizedPrincipalsFile none
+
+        #AuthorizedKeysCommand none
+        #AuthorizedKeysCommandUser nobody
+
+        # For this to work you will also need host keys in /etc/ssh/ssh_known_hosts
+        #HostbasedAuthentication no
+        # Change to yes if you don't trust ~/.ssh/known_hosts for
+        # HostbasedAuthentication
+        #IgnoreUserKnownHosts no
+        # Don't read the user's ~/.rhosts and ~/.shosts files
+        #IgnoreRhosts yes
+
+        # To disable tunneled clear text passwords, change to no here!
+        #PasswordAuthentication yes
+        #PermitEmptyPasswords no
+
+        # Change to yes to enable challenge-response passwords (beware issues with
+        # some PAM modules and threads)
+        [ChallengeResponseAuthentication]="no"
+
+        # Kerberos options
+        #KerberosAuthentication no
+        #KerberosOrLocalPasswd yes
+        #KerberosTicketCleanup yes
+        #KerberosGetAFSToken no
+
+        # GSSAPI options
+        #GSSAPIAuthentication no
+        #GSSAPICleanupCredentials yes
+        #GSSAPIStrictAcceptorCheck yes
+        #GSSAPIKeyExchange no
+
+        # Set this to 'yes' to enable PAM authentication, account processing,
+        # and session processing. If this is enabled, PAM authentication will
+        # be allowed through the ChallengeResponseAuthentication and
+        # PasswordAuthentication.  Depending on your PAM configuration,
+        # PAM authentication via ChallengeResponseAuthentication may bypass
+        # the setting of "PermitRootLogin without-password".
+        # If you just want the PAM account and session checks to run without
+        # PAM authentication, then enable this but set PasswordAuthentication
+        # and ChallengeResponseAuthentication to 'no'.
+        UsePAM yes
+
+        #AllowAgentForwarding yes
+        #AllowTcpForwarding yes
+        #GatewayPorts no
+        X11Forwarding yes
+        #X11DisplayOffset 10
+        #X11UseLocalhost yes
+        #PermitTTY yes
+        PrintMotd no
+        #PrintLastLog yes
+        #TCPKeepAlive yes
+        #PermitUserEnvironment no
+        #Compression delayed
+        #ClientAliveInterval 0
+        #ClientAliveCountMax 3
+        #UseDNS no
+        #PidFile /var/run/sshd.pid
+        #MaxStartups 10:30:100
+        #PermitTunnel no
+        #ChrootDirectory none
+        #VersionAddendum none
+
+        # no default banner path
+        #Banner none
+
+        # Allow client to pass locale environment variables
+        AcceptEnv LANG LC_*
+
+        # override default of no subsystems
+        Subsystem	sftp	/usr/lib/openssh/sftp-server
+
+        # Example of overriding settings on a per-user basis
+        #Match User anoncvs
+        #	X11Forwarding no
+        #	AllowTcpForwarding no
+        #	PermitTTY no
+        #	ForceCommand cvs server
+
+
         [DebianBanner]="no"
         [ClientAliveInterval]="10m"  # Adjust this to your desired interval
         [X11Forwarding]="no"
         [MaxAuthTries]="4"
-        [LogLevel]="VERBOSE"
         [Port]="$PORT"
         [LoginGraceTime]="20"
         [MaxSessions]="9"
         [GSSAPIAuthentication]="no"
         [AllowAgentForwarding]="yes"
-        [ForwardAgent]="yes"
         # [HostBasedAuthentification]="no"
         # [StrictHostKeyChecking]="ask"
         [AllowTcpForwarding]="yes"
@@ -493,12 +636,8 @@ harden_sshd_config() {
         [PermitUserEnvironment]="no"
         [UsePrivilegeSeparation]="no" # Setting privilege separation helps to secure remote ssh access. Once a user is authenticated the sshd daemon creates a child process which has the privileges of the authenticated user and this then handles incoming network traffic. The aim of this is to prevent privilege escalation through the initial root process.
         [PermitEmptyPasswords]="no"
-        # PermitTunnel no
+        [PermitTunnel]="no"
         # ChallengeResponseAuthentication # Don't know if usefull or not
-    )
-
-        settings_ssh_daemon=(
-        [Port]="$PORT"
     )
 
     # Loop through each setting and apply changes
