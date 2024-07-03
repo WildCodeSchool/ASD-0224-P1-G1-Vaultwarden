9e2e4fbc890b73734fe9313e186fcbf11efed96a
Yann-T <yanntejero@gmail.com>
2024-07-03 16:23:03 +0200

    Fix

--- scripts/nginx_conf.sh
+++ scripts/nginx_conf.sh
@@ -17,8 +17,6 @@ fi
 
 # Variables
 nginx_conf="/etc/nginx/sites-available/default"
-apache_port_conf="/etc/apache2/ports.conf"
-apache_vaultwarden="/etc/apache2/route/vers/vaultwarden"
 domaine="notre.domaine.v6.rocks"  # Replace with our domaine
 dynv6_token="" # Penser a resegner notre token
 
@@ -26,20 +24,20 @@ dynv6_token="" # Penser a resegner notre token
 echo "Updating package lists..."
 apt update
 
-# Install Nginx and Apache if not already installed
+# Install Nginx if not already installed
 echo "Installing Nginx ..."
-apt install -y nginx apache2 apt-utils autoconf automake build-essential git libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre++-dev libtool libxml2-dev libyajl-dev pkgconf wget zlib1g-dev certbot
-echo -e "Installation des packages pour Nginx Apache et Modprobe realisee.\n" >> $dir
+apt install -y nginx  apt-utils autoconf automake libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre++-dev libtool libxml2-dev libyajl-dev pkgconf wget zlib1g-dev certbot
+echo -e "$install_date - Installation des packages pour Nginx et Modprobe realisee.\n" >> $dir
 
-# Mise en pause de Nginx et Apache pendant leur configuration / Stop Nginx and Apache2 before installing and setting up the config for modsecurity
+# Mise en pause de Nginx pendant leur configuration / Stop Nginx and Apache2 before installing and setting up the config for modsecurity
 systemctl stop nginx
-systemctl stop apache2
+
 
 ##################################
 ########### Certbot  WIP ##############
 ##################################
 certbot certonly --manual --preferred-challenges dns -d "*.$domaine" -d $domaine
-echo -e "Certificat recupere.\n" >> $dir
+echo -e "$install_date - Certificat recupere.\n" >> $dir
 
 # Configure Nginx
 echo "Configuring Nginx as a reverse proxy..."
@@ -62,16 +60,7 @@ server {
     }
 }
 EOF
-echo -e "Parametrage Nginx reverse proxy et preparation Modprobe.\n" >> $dir
-
-# Configure Apache to listen on port 8080
-echo "Configuring Apache to listen on port 8080..."
-sed -i 's/Listen 80/Listen 8080/' "$apache_port_conf"
-
-# Update Apache Virtual Host to use port 8080
-echo "Updating Apache Virtual Host configuration..."
-sed -i 's/<VirtualHost \*:80>/<VirtualHost \*:8080>/' "$apache_vaultwarden"
-echo -e "Configuration de Apache OK.\n" >> $dir
+echo -e "$install_date - Parametrage Nginx reverse proxy et preparation Modprobe.\n" >> $dir
 
 # Recuperation et installation de ModSecurity
 git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity /usr/local/src
@@ -92,7 +81,7 @@ cd nginx-$nginx_vers/
 ./configure --with-compat --add-dynamic-module=../ModSecurity-nginx
 make modules
 cp /usr/local/src/nginx-$nginx_vers/objs/ngx_http_modsecurity_module.so /etc/nginx/modules/
-echo -e "Recuperation et compilation de Modprobe OK.\n" >> $dir
+echo -e "$install_date - Recuperation et compilation de Modprobe OK.\n" >> $dir
 
 # Ajout du load module a la conf Nginx
 sed -i '/include \/etc\/nginx\/modules-enabled\/\*\.conf;/a load_module \/etc\/nginx\/modules\/ngx_http_modsecurity_module.so;' /etc/nginx/nginx.conf
@@ -113,17 +102,15 @@ wget https://github.com/SpiderLabs/owasp-modsecurity-crs/archive/refs/tags/v3.2.
 tar -xzvf v3.2.0.tar.gz
 cd owasp-modsecurity-crs-3.2.0/
 cp crs-setup.conf.example crs-setup.conf
-echo -e "Application des regles de securite de base OWASP.\n" >> $dir
+echo -e "$install_date - Application des regles de securite de base OWASP.\n" >> $dir
 
 #################################################################
 ##### choix du site a surveiller/ liste d esceptions ajouter ####
 #################################################################
 
-# Restart services and enable Nginx and Apache2
-echo "Restarting Nginx and Apache services..."
+# Restart services and enable Nginx
+echo "Restarting Nginx service..."
 systemctl enable nginx
-systemctl enable apache2
 systemctl restart nginx
-systemctl restart apache2
 
 echo "Nginx as a reverse proxy to Apache is now set up."
