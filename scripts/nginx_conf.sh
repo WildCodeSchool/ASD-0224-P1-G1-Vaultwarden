2e0d61e12834307f0eb8b1a9ce6fa3e2211ca1e5
Antoine Aibeche <antoine.aibeche@iscparis.com>
2024-07-05 10:07:04 +0200

    modif pauses

--- scripts/nginx_conf.sh
+++ scripts/nginx_conf.sh
@@ -32,13 +32,12 @@ echo -e "$install_date - Installation des packages pour Nginx et Modprobe realis
 # Mise en pause de Nginx pendant leur configuration / Stop Nginx and Apache2 before installing and setting up the config for modsecurity
 systemctl stop nginx
 
-
 ##################################
 ########### Certbot  WIP ##############
 ##################################
-certbot certonly --manual --preferred-challenges dns -d "*.$domaine" -d $domaine
+certbot certonly --manual --register-unsafely-without-email --preferred-challenges dns -d "*.$domaine" -d $domaine
 echo -e "$install_date - Certificat recupere.\n" >> $dir
-echo -e "\nFin install Certbot\n" && pause
+echo -e "\n" && read -p "\nFin install Certbot\n" && echo -e "\n"
 
 # Configure Nginx
 echo "Configuring Nginx as a reverse proxy..."
@@ -62,7 +61,7 @@ server {
 }
 EOF
 echo -e "$install_date - Parametrage Nginx reverse proxy et preparation Modprobe.\n" >> $dir
-echo -e "\nFin modif conf Nginx\n" && pause
+echo -e "\n" && read -p "\nFin modif conf Nginx\n" && echo -e "\n"
 
 # Recuperation et installation de ModSecurity
 rm -rf /usr/local/src/ModSecurity/
@@ -70,28 +69,31 @@ git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/M
 cd /usr/local/src/ModSecurity/
 git submodule init
 git submodule update --remote
-echo -e "\nFin recup git Modsecurity\n" && pause
+echo -e "\n" && read -p "\nFin recup git Modsecurity\n" && echo -e "\n"
 ./build.sh
+echo -e "\n" && read -p "\nFin build.sh Modsecurity\n" && echo -e "\n"
 ./configure
+echo -e "\n" && read -p "\nFin configure.sh Modsecurity\n" && echo -e "\n"
 make
+echo -e "\n" && read -p "\nFin make Modsecurity\n" && echo -e "\n"
 make install
-echo -e "\nFin make install Modsecurity\n" && pause
+echo -e "\n" && read -p "\nFin make install Modsecurity\n" && echo -e "\n"
 
 # Module ModSecurity pour Nginx
 rm -rf /usr/local/src/ModSecurity-nginx
 git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git /usr/local/src/ModSecurity-nginx
 cd /usr/local/src/ModSecurity-nginx
-echo -e "\nFin git clone Modsecurity-nginx\n" && pause
+echo -e "\n" && read -p "\nFin git clone Modsecurity-nginx\n" && echo -e "\n"
 nginx_vers=$(nginx -v 2>&1 | awk -F'/' '{print $2}' | awk -F' ' '{print $1}')
 wget http://nginx.org/download/nginx-$nginx_vers.tar.gz
 tar zxvf nginx-$nginx_vers.tar.gz
 cd nginx-$nginx_vers/
 ./configure --with-compat --add-dynamic-module=../ModSecurity-nginx
 make modules
-echo -e "\nFin make module Modsecurity-nginx\n" && pause
+echo -e "\n" && read -p "\nFin make module Modsecurity-nginx\n" && echo -e "\n"
 cp /usr/local/src/ModSecurity-nginx/nginx-$nginx_vers/objs/ngx_http_modsecurity_module.so /etc/nginx/modules/
 echo -e "$install_date - Recuperation et compilation de Modprobe OK.\n" >> $dir
-echo -e "\nFin copie Modsecurity-nginx dans modules Nginx\n" && pause
+echo -e "\n" && read -p "\nFin copie Modsecurity-nginx dans modules Nginx\n" && echo -e "\n"
 
 # Ajout du load module a la conf Nginx
 sed -i '/include \/etc\/nginx\/modules-enabled\/\*\.conf;/a load_module \/etc\/nginx\/modules\/ngx_http_modsecurity_module.so;' /etc/nginx/nginx.conf
@@ -101,10 +103,10 @@ mkdir /etc/nginx/modsec
 wget -P /etc/nginx/modsec/ https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/modsecurity.conf-recommended
 mv /etc/nginx/modsec/modsecurity.conf-recommended /etc/nginx/modsec/modsecurity.conf
 cp /usr/local/src/ModSecurity/unicode.mapping /etc/nginx/modsec/
-echo -e "\nRecuperation de la conf recomended\n" && pause
+echo -e "\n" && read -p "\nRecuperation de la conf recomended\n" && echo -e "\n"
 
 sed -i 's/DetectionOnly/On/' /etc/nginx/modsec/modsecurity.conf
-echo -e "\nModif config modsecurity\n" && pause
+echo -e "\n" && read -p "\nModif config modsecurity\n" && echo -e "\n"
 
 echo -e "Include \"/etc/nginx/modsec/modsecurity.conf\"" > /etc/nginx/modsec/main.conf
 
@@ -112,12 +114,12 @@ echo -e "Include \"/etc/nginx/modsec/modsecurity.conf\"" > /etc/nginx/modsec/mai
 cd /usr/local/src
 wget https://github.com/SpiderLabs/owasp-modsecurity-crs/archive/refs/tags/v3.2.0.tar.gz
 tar -xzvf v3.2.0.tar.gz
-echo -e "\nRecuperation et decompression regles OWASP\n" && pause
+echo -e "\n" && read -p "\nRecuperation et decompression regles OWASP\n" && echo -e "\n"
 
 cd owasp-modsecurity-crs-3.2.0/
 cp crs-setup.conf.example crs-setup.conf
 echo -e "$install_date - Application des regles de securite de base OWASP.\n" >> $dir
-echo -e "\nCopie regles OWASP\n" && pause
+echo -e "\n" && read -p "\nCopie regles OWASP\n" && echo -e "\n"
 
 #################################################################
 ##### choix du site a surveiller/ liste d esceptions ajouter ####
