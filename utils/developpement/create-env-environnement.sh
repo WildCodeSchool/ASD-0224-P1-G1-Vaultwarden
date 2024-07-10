d0234e8976f1684032cee6e035945df6fb65244d
Vehementz <vale-tudo91@sfr.fr>
2024-07-10 14:37:16 +0200

    dev environnement improvement

--- utils/developpement/create-env-environnement.sh
+++ utils/developpement/create-env-environnement.sh
@@ -11,8 +11,22 @@ cp /etc/ssh/sshd_config $workspace/sshd_config_bakcup
 cp /etc/ssh/ssh_config $workspace/ssh_config_bakcup
 
 touch reset_script.sh
+touch developpement-script.sh
+
 cat <<EOF > reset_script.sh
 #! /bin/bash
 cp $workspace/sshd_config_bakcup /etc/ssh/sshd_config 
 cp $workspace/ssh_config_bakcup /etc/ssh/ssh_config 
 EOF
+
+chmod +x reset_script.sh
+chmod +x developpement-script.sh
+
+touch show_ssh_files.sh 
+cat <<EOF > show_ssh_files.sh 
+#! /bin/bash
+cat /etc/ssh/sshd_config
+cat /etc/ssh/ssh_config
+EOF
+
+chmod +x show_ssh_files.sh
